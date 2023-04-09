cd48f82a0178c8b6b6cb4bb06f851db9f3aa446b
Chase <chambers.chase@gmail.com>
2023-04-09 15:30:32 -0400

    Changes adding answers in array to buttons

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
+var questions = document.querySelector('.question');
+var startButton = document.querySelector('.start');
+var resetButton = document.querySelector('.reset');
+var answers = document.querySelector('.answer')
+var timer = document.querySelector('.timer')
 
 
-// START BUTTON STARTS TIMER
+// START BUTTON STARTS TIMER AND QUIZ
 
+var timeRemaining = 60;
+// var countdown = setInterval(function() {
+//     clearInterval(countdown);    
+//     timeRemaining--;
+//    timer.textContent = timeRemaining;
+//     if (timeRemaining < 1) {
+//       clearInterval();
+//          timer.textContent = 'PENCILS DOWN!'
+//        }
+//     }, 1000);
+
+var questionsList = [
+    {quizAnswer: 'Correct', correct: true},
+    {quizAnswer: 'Incorrect', correct: false},
+    {quizAnswer: 'Incorrect', correct: false},
+    {quizAnswer: 'Incorrect', correct: false}
+    ];
+
+
+
+   startButton.addEventListener('click', function(){
+     document.querySelector('.question-box').style.display ='block';
+    // questionsList.forEach((answer) => {
+    //     answers.textContent=answer.quizAnswer;
+ questionsList.forEach(i => {
+    var button = document.createElement("button");
+    button.innerText = i.quizAnswer;
+    answers.appendChild(button);
+ })
+});
 
 // RESET BUTTON RESETS QUIZ BACK TO ZERO
-
+resetButton.addEventListener('click', function(){
+    document.querySelector('.question-box').style.display ='none';
+    clearInterval(countdown);
+    timer.textContent = 60;
+    });
 
 // HIGHSCORE TABLE HOLDS SCORE AND INITIALS - MOVES INFO DEPENDING ON SCORE PLACEMENT
 
 
 // QUESTIONS AND ANSWERS
 
-var questions = document.querySelector('.question');
-
-document.querySelector('.start').addEventListener('click', function(){
-alert('hello');
-});
-
-document.querySelector('.reset').addEventListener('click', function(){
-    alert('goodbye');
-    });
+answers.addEventListener('click', function(){
+    if(questionsList.correct === true) {
+        console.log("That's right");
+    } else {
+        console.log("That's not right");
+    }
+})

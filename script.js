674e7733349cd3d14cdf494a21c06cb5e420371a
Chase <chambers.chase@gmail.com>
2023-04-09 17:19:30 -0400

    soft reboot on code

--- script.js
+++ script.js
@@ -4,56 +4,36 @@ var resetButton = document.querySelector('.reset');
 var answers = document.querySelector('.answer')
 var timer = document.querySelector('.timer')
 
+//START BUTTON
+startButton.addEventListener('click', function() {
 
-// START BUTTON STARTS TIMER AND QUIZ
-
-var timeRemaining = 60;
-// var countdown = setInterval(function() {
-//     clearInterval(countdown);    
-//     timeRemaining--;
-//    timer.textContent = timeRemaining;
-//     if (timeRemaining < 1) {
-//       clearInterval();
-//          timer.textContent = 'PENCILS DOWN!'
-//        }
-//     }, 1000);
-
-var questionsList = [
-    {quizAnswer: 'Correct', correct: true},
-    {quizAnswer: 'Incorrect', correct: false},
-    {quizAnswer: 'Incorrect', correct: false},
-    {quizAnswer: 'Incorrect', correct: false}
-    ];
-
-
-
-   startButton.addEventListener('click', function(){
-     document.querySelector('.question-box').style.display ='block';
-    // questionsList.forEach((answer) => {
-    //     answers.textContent=answer.quizAnswer;
- questionsList.forEach(i => {
-    var button = document.createElement("button");
-    button.innerText = i.quizAnswer;
-    answers.appendChild(button);
- })
-});
-
-// RESET BUTTON RESETS QUIZ BACK TO ZERO
-resetButton.addEventListener('click', function(){
-    document.querySelector('.question-box').style.display ='none';
-    clearInterval(countdown);
-    timer.textContent = 60;
-    });
-
-// HIGHSCORE TABLE HOLDS SCORE AND INITIALS - MOVES INFO DEPENDING ON SCORE PLACEMENT
-
-
-// QUESTIONS AND ANSWERS
-
-answers.addEventListener('click', function(){
-    if(questionsList.correct === true) {
-        console.log("That's right");
-    } else {
-        console.log("That's not right");
-    }
 })
+
+//RESET BUTTON
+
+resetButton.addEventListener('click', function() {
+
+})
+
+//QUESTION SECTION
+var showQuestions = function() {
+
+}
+
+//ANSWER SECTION
+var showAnswers = function() {
+
+}
+
+//TIMER SECTION
+var countdown = function(){
+
+}
+
+//HIGHSCORE SECTION
+var highscore = function(){
+
+}
+
+
+

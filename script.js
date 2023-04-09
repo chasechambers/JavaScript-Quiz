a5bb3415f641ad3876cc2b55a66fb423a3812565
Chase <chambers.chase@gmail.com>
2023-04-09 19:57:54 -0400

    Current updates - Saving

--- script.js
+++ script.js
@@ -3,32 +3,59 @@ var startButton = document.querySelector('.start');
 var resetButton = document.querySelector('.reset');
 var answers = document.querySelector('.answer')
 var timer = document.querySelector('.timer')
-
+var btn = document.querySelector('.btn')
+var startingTime = 60;
 //START BUTTON
-startButton.addEventListener('click', function() {
 
-})
+startButton.addEventListener('click', function() {
+showQuestions();
+showAnswers();
+countdown();
+});
 
 //RESET BUTTON
 
 resetButton.addEventListener('click', function() {
-
+clearInterval(countdownClock);
+timer.textContent = startingTime;
+var startingTime = 60;
 })
 
 //QUESTION SECTION
-var showQuestions = function() {
 
-}
+var showQuestions = function() {
+    document.querySelector('.question').textContent='This is a test question.'
+};
+
+
+
+
 
 //ANSWER SECTION
+
+var answerList = ['a', 'b', 'c', 'd']
 var showAnswers = function() {
+    answerList.forEach((answer)=> {
+        document.querySelector('.answer').textContent = answer;
+    });
+};
+
+var answerElection = function(){
 
 }
+
 
 //TIMER SECTION
-var countdown = function(){
 
-}
+const countdownClock = setInterval(countdown, 1000);
+
+function countdown() {
+    timer.textContent=startingTime;
+    startingTime--;
+     if (startingTime < 1 ) {
+         timer.textContent = 'PENCILS DOWN! 📝'
+     };
+    };
 
 //HIGHSCORE SECTION
 var highscore = function(){
@@ -36,4 +63,5 @@ var highscore = function(){
 }
 
 
-
+// localStorage.getItem(finalScore);
+// localStorage.setItem(finalScore, score)
